d0ca0f25838bcd2ce8b9cee9d5051a79b6ad6c33
gagandeep1983 <gagangarg.deep@gmail.com>
2019-02-14 23:54:24 -0500

    change Configuration file

--- WFM/Startup.cs
+++ WFM/Startup.cs
@@ -28,7 +28,8 @@ namespace WFMWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = @"Server=happy;Database=Work_Flow_Management;Trusted_Connection=True;ConnectRetryCount=0";
+            //var connection = @"Server=happy;Database=Work_Flow_Management;Trusted_Connection=True;ConnectRetryCount=0";
+            var connection = @"Server=103.48.50.146,1232;Database=Work_Flow_Management;ConnectRetryCount=0;User Id=wfm;Password=Wfm@123#;";
             services.AddDbContext<WFMContext>
                 (options => options.UseSqlServer(connection));
             services.AddTransient<IUserService, UserService>();
