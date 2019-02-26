a0fc4b27e343a7445defba3048adf699df2d6f7a
gagandeep1983 <gagangarg.deep@gmail.com>
2019-02-26 08:37:06 -0500

    implement swagger

--- WFM/Startup.cs
+++ WFM/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Options;
 using WFM.Entity;
 using WFM.Services.Interfaces;
 using WFM.Services.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WFMWebAPI
 {
@@ -36,6 +37,12 @@ namespace WFMWebAPI
             services.AddMvc();
     
             services.AddAutoMapper();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,17 @@ namespace WFMWebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             app.UseMvc();
         }
