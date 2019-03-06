9dd15fa50cd4f0c2dda33c5301d41662fb582c3d
gagandeep1983 <gagangarg.deep@gmail.com>
2019-03-06 00:12:08 -0500

    commit JWT

---
+++ WFM/DTOs/LoginDTOs/LoginRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace WFM.WebAPI.DTOs.LoginDTOs
+{
+    
+    public class LoginRequest
+    {
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string password { get; set; }
+        
+
+    }
+}
